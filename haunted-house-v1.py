b34120be36cf9cfd6b303640da04febff2406d39
Tyler Fink <tsfink182@stevenscollege.edu>
2019-02-01 21:45:04 -0500

    First commit

---
+++ haunted-house-v1.py
@@ -0,0 +1,32 @@
+# where i want to create my dictionary in order to reference the opening scene
+# and also move to and from scenes
+class House(object):
+    pass
+
+# where the game "logic" will be handled
+class Engine(object):
+    pass
+
+# where you will awake up in this dark scary hosue
+class MainBedroom(Rooms):
+    pass
+
+# phase 2
+class Hallway(Rooms):
+    pass
+
+# phase 3
+class SecondBedroom(Rooms):
+    pass
+
+# phase 4
+class Kitchen(Rooms):
+    pass
+
+# phase 5
+class LivingRoom(Rooms):
+    pass
+
+# phase 6
+class Basement(Rooms):
+    pass
